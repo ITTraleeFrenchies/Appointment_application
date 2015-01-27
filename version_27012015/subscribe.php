012531f43023a01d687065617120eb7796363b2d
kyalha <angel079@hotmail.fr>
2015-01-27 16:58:51 +0000

    Form really finished for subscribe
    New data added corresponding to the last meeting's expectation.

--- version_27012015/subscribe.php
+++ version_27012015/subscribe.php
@@ -15,23 +15,25 @@
 			function setCursorPos(){
 					setCaretToPos(document.getElementById("comment"), 0);
 			}
-		function setSelectionRange(input, selectionStart, selectionEnd) {
-				if (input.setSelectionRange) {
-				input.focus();
-				input.setSelectionRange(selectionStart, selectionEnd);
-			}
-			else if (input.createTextRange) {
-				var range = input.createTextRange();
-				range.collapse(true);
-				range.moveEnd('character', selectionEnd);
-				range.moveStart('character', selectionStart);
-				range.select();
+			function setSelectionRange(input, selectionStart, selectionEnd) {
+					if (input.setSelectionRange) {
+					input.focus();
+					input.setSelectionRange(selectionStart, selectionEnd);
 				}
-		}
-
+				else if (input.createTextRange) {
+					var range = input.createTextRange();
+					range.collapse(true);
+					range.moveEnd('character', selectionEnd);
+					range.moveStart('character', selectionStart);
+					range.select();
+					}
+			}
 			function setCaretToPos (input, pos) {
 			setSelectionRange(input, pos, pos);
 			}
+
+
+		/* ======== DATE BIRTH ======== */
 				
 			    var datefield=document.createElement("input");
 			    datefield.setAttribute("type", "date");
@@ -76,8 +78,8 @@
 					    <input type="text" name="lastname" maxlength="30"/>
 						<br>
 						<br>
-							<label for="datebirth">Date of birth</label>
-							<input type="date" id="datebirth" name="birthday" size="20"/>
+						<label for="datebirth">Date of birth</label>
+						<input type="date" name="datebirth" size="20"/>
 						<br>
 						<br>
 						<label for="address1">Adress (first line)</label>
@@ -88,10 +90,14 @@
 					    <input type="text" name="address2" maxlength="100"/>
 						<br>
 						<br>
+						<label for="city">City </label>
+					    <input type="text" name="city" maxlength="35"/>
+						<br>
+						<br>
 						<label for="counties">County</label>
 						<label>
-							<select name="counties" name="selectCounty">
-								<option value="select">Select a county </option>
+							<select name="selectCounty">
+								<option  selected disabled value="select">Select a county </option>
 							   <option value="Cork"> Cork</option>
 							   <option value="Galway"> Galway</option>
 							   <option value="Mayo"> Mayo</option>
@@ -130,7 +136,7 @@
 						<br>
 						<label for="courses">Select your course</label>
 						<label>
-							 <select id="courses">
+							 <select name="courses">
 								<option selected disabled>Select Department</option>
 						       <?php
 								$select = file_get_contents('files/Courses_list.txt');
@@ -148,7 +154,7 @@
 						<label for="disability">Indicate if you are</label>
 						<label>
 							<select name="disability" name="selectDisability">
-								<option value="select">Select a term </option>
+								<option  selected disabled value="select">Select a term </option>
 							   <option value="none"> None</option>
 							   <option value="none"> Student with a disability</option>
 							   <option value="none"> Student with a learning difficulty</option>
@@ -160,35 +166,38 @@
 						<br>	
 					</div>
 					<div class="part_check_align">
-						<label for="disability_support">Please indicate below the nature of your disability by ticking the appropriate box(es)</label>
+						<label>Please indicate below the nature of your disability by ticking the appropriate box(es)</label>
 						<br>
-						<input type="checkbox" name="PA" value="1"><label class="checkbox-label" for="PA"> Physical Disability ambulant</label> 
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="1"><label class="checkbox-label" for="PA"> Physical Disability ambulant</label> 
 						<br>
-						<input type="checkbox" name="VI" value="2"><label class="checkbox-label" for="VI"> Visual Impairment</label> 
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="2"><label class="checkbox-label" for="VI"> Visual Impairment</label> 
 						<br>
-						<input type="checkbox" name="DF" value="3"><label class="checkbox-label" for="DF"> Deaf</label> 
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="3"><label class="checkbox-label" for="DF"> Deaf</label> 
 						<br>
-						<input type="checkbox" name="BL" value="4"><label class="checkbox-label" for="BL"> Blind
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="4"><label class="checkbox-label" for="BL"> Blind
 						<br>
-						<input type="checkbox" name="HD" value="5"><label class="checkbox-label" for="HD"> Hard of hearing
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="5"><label class="checkbox-label" for="HD"> Hard of hearing
 						<br>
-						<input type="checkbox" name="MH" value="6"><label class="checkbox-label" for="MH"> Mental Health
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="6"><label class="checkbox-label" for="MH"> Mental Health
 						<br>
-						<input type="checkbox" name="MS" value="7"><label class="checkbox-label" for="MS"> Neurological (e.g. Acquired Brain injury)
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="7"><label class="checkbox-label" for="MS"> Neurological (e.g. Acquired Brain injury)
 						<br>
-						<input type="checkbox" name="HC" value="8"><label class="checkbox-label" for="HC"> Significant Health Condition
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="8"><label class="checkbox-label" for="HC"> Significant Health Condition
 						<br>
-						<input type="checkbox" name="PN" value="9"><label class="checkbox-label" for="PN"> Physical Disability non-ambulant (wheelchair user)
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="9"><label class="checkbox-label" for="PN"> Physical Disability non-ambulant (wheelchair user)
 						<br>
-						<input type="checkbox" name="DY" value="4"><label class="checkbox-label" for="DY"> Specific Learning Difficulty(e.g. dyslexia, dyspraxia,dyscalculia)
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="4"><label class="checkbox-label" for="DY"> Specific Learning Difficulty(e.g. dyslexia, dyspraxia,dyscalculia)
 						<br>
-						<input type="checkbox" name="OT" value="4"><label class="checkbox-label" for="v"> Other
+						<input type="checkbox" name="disabilityS[]" name="disabilityS" value="4"><label class="checkbox-label" for="v"> Other
 						<br>
-						</li>
+						<br>
+						<label for="contact">Contact number</label>
+					    <input type="text" name="contact" maxlength="14"/>
+						<br>
 						<br>
 						<label for="comment" maxlength="500">Please specify disability supports required</label>
 						<br>
-						 <textarea id = "comment" rows = "3"cols = "80" value="insert your comment here" onclick="setCursorPos()">
+						 <textarea id ="comment" name = "comment" rows = "3"cols = "80" value="insert your comment here" onclick="setCursorPos()">
 		              	</textarea>
 		             </div>
 						<br>
