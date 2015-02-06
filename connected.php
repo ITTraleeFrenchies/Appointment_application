2289f30c72ebb0ae48753c7773f834127a6c8374
GloveAndBoots <aurelien.bigois@gmail.com>
2015-02-06 16:55:58 +0000

    Update connect.php
    Added a shit load of javascript and php HELL YEAH

--- connected.php
+++ connected.php
@@ -1,28 +1,45 @@
 <?php
 /* =================  We include the script in order to retrieve data  ====================== */
 	include('extract_busy_time.php'); 
-
+		$day_meeting="";
+		$timestart_meeting="";
+		$timeend_meeting="";
 		function display($user, $password, $file){
 			loadUser($user, $password, $file);
 			$select = file_get_contents($file);
 			$lines = explode("\n", $select);
 			echo "Busy Times : <br>";
+
+			$incr_array_busy_times=0;
+			$array_busy_times="";
 			foreach($lines as $line){
 				if(substr( $line, 0, 8 ) == "FREEBUSY" ){
 					$dates = explode(":",$line);
 					$type = explode("=",$dates[0]);
 					if($type[1] == "BUSY" || $type[1] == "BUSY-UNAVAILABLE") $type[1]="Busy";
 					if($type[1] == "BUSY-TENTATIVE") $type[1]="Waiting for validation";
-					echo "Day : ".substr($dates[1],6,2)."/".substr($dates[1],4,2)."/".substr($dates[1],0,4).
-					" Time : ".substr($dates[1],9,2).".".substr($dates[1],11,2)." - ".substr($dates[1],26,2).".".substr($dates[1],28,2).
-					" Status : ".$type[1]."<br>";
+					$day_meeting=substr($dates[1],6,2)."/".substr($dates[1],4,2)."/".substr($dates[1],0,4);
+					
+					// ============= hours and minutes concatenated of each busy time ==========
+					$timestart_meeting=substr($dates[1],9,4);
+					$timeend_meeting=substr($dates[1],26,4);
+					
+					// ============= hours and minutes separated of each busy time ==========
+					/*$timestart_meeting=substr($dates[1],9,2).".".substr($dates[1],11,2);
+					$timeend_meeting=substr($dates[1],26,2).".".substr($dates[1],28,2);*/
+					
+					// =============== We put in an array all the busy meeting ================
+					echo "Day : ".$day_meeting." Time : ".$timestart_meeting." - ".$timeend_meeting." Status : ".$type[1]."<br>";
+					$array_busy_times[$incr_array_busy_times] = $timestart_meeting .'-'.$timeend_meeting;
+					$incr_array_busy_times++;
 				}
 			}
+			
+			/*foreach($array_busy_times as $x => $x_value) {
+				 echo $x . ' - ' . $x_value.'<br>';
+			}*/
+			
 		}
-
-
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,12 +52,55 @@
 
 		<script type="text/javascript">
 			function getdata(){
-				var myval = document.getElementById("service").value;
+				var service = document.getElementById("service").value;
 				var i ="";	
-				if(myval==1){ i = "<?php echo display('dyslexia','testproj15dysl','content_dyslexia.txt'); ?>";}
-				else if (myval==2){ i = "<?php echo display('counsellor','testproj15coun','content_counsellor.txt'); ?>";}
-				else if (myval==3){ i = "<?php echo display('access','testproj15accs','content_access.txt'); ?>";}
-				document.getElementById("display").innerHTML=i;
+				if(service=="Dyslexia Support Office"){ i = "<?php echo display('dyslexia','testproj15dysl','content_dyslexia.txt'); ?>";}
+				else if (service=="Counselling"){ i = "<?php echo display('counsellor','testproj15coun','content_counsellor.txt'); ?>";}
+				else if (service=="Access Office"){ i = "<?php echo display('access','testproj15accs','content_access.txt'); ?>";}
+				document.getElementById("display-service").innerHTML=i;
+				
+				
+				var choice_date = document.getElementById("date").value;
+				var choice_start_time = document.getElementById("time_start").value;
+				var choice_end_time = document.getElementById("time_end").value;
+				/*document.getElementById("display-date").innerHTML=choice_date;
+				document.getElementById("display-time-start").innerHTML=choice_start_time;
+				document.getElementById("display-time-end").innerHTML=choice_end_time;*/
+				
+				// =========== We put all the values for the select of end time as disabled ============
+				var select_time_end = document.getElementById("time_end");
+				var j;
+				for (j = 0; j <  select_time_end.length; j++) {
+							select_time_end[j].disabled = true ;
+				}
+				
+				// =========== We disable all the values of end time before start time ============
+					var concat_time_start = choice_start_time.substr(0, 2) + choice_start_time.substr(3, 2);
+					var k;
+					var last_select_time_end;
+					var putfirstSelected = true;
+					for (k = 0; k <  select_time_end.length; k++) {
+						var concat_time_end = select_time_end.options[k].text.substr(0, 2) + select_time_end.options[k].text.substr(3, 2);
+						if(concat_time_end <= concat_time_start){
+							select_time_end[k].disabled = true ;
+						}else {
+							select_time_end[k].disabled = false ;
+							if(putfirstSelected == true){
+								select_time_end[k].selected = true;
+								putfirstSelected = false;
+							}
+						}
+					}
+				
+				
+				/*var array_busy_times_js = [<?php echo '"'.implode('","', $array_busy_times).'"' ?>];			
+				var l=0;
+				var tout="";
+				for(l =0; l < array_busy_times_js.length; l++){
+					tout += array_busy_times_js[l];
+				}				
+				alert("coucou" + tout);*/
+				
 			}
 		</script>
 	</head>
@@ -58,12 +118,12 @@
 
 					<article class="tabs">
 						<div class="part_align">
-						<form action="" method="post" name="form" id="id_form">
+						<form action="" method="post" name="form" id="id_form" onchange="getdata();">
 							<section id="tab1">
 									<p>
 										<h2><a href="#tab1">Make an appointment</a></h2>
 										<label for="service">Select service</label>
-										<select name="service">
+										<select name="service" id="service">
 										  <option value="Health Centre">Health Centre</option>
 										  <option value="Careers Office">Careers Office</option>
 										  <option value="Chaplaincy">Chaplaincy</option>
@@ -77,6 +137,10 @@
 										</select>  
 										  <br>
 										  <br>
+										  <div id="display-service"> </div>
+										  <div id="display-date"></div>
+										  <div id="display-time-start"></div>
+										  <div id="display-time-end"></div>
 										<br>
 											<label for="date">Date</label>		
 										<select id='date'>
@@ -91,7 +155,7 @@
 												 //	$date=date('l jS \of F Y ',strtotime('+'.$day .' days'));
 												 	if($day_letters != 'Sat' && $day_letters != 'Sun'){
 												 		 //$date = date_format($date,'l jS \of F Y ');
-												 		echo'<option value=" ='. $date . '">' . $date .'</option>';
+												 		echo'<option value="'. $date . '">' . $date .'</option>';
 												 	}
 													$day++;
 												 }
@@ -100,7 +164,7 @@
 										
 										<br>	
 										<label for="start">Start</label>		
-										<select id='time_start'>
+										<select id="time_start">
 											<?php 
 											for($hour = 8; $hour < 17; $hour++){
 												$minutes =0;
@@ -109,7 +173,7 @@
 												 	if($hour <=10){
 												 		$hour = str_pad($hour, 2, '0', STR_PAD_LEFT);
 												 	}
-												 	 echo'<option value=" ='. $hour . ' ":" '.$minutes.'">' . $hour .':' .$minutes . '</option>';
+												 	 echo'<option value="'. $hour .'.'.$minutes.'">' . $hour .'.' .$minutes . '</option>';
 												 	 $minutes+=15;
 												 	}
 												} 
@@ -119,19 +183,21 @@
 										<label for="End">End</label>	
 										<select id='time_end'>
 											<?php 
+											$minutes =15;
 											for($hour = 8; $hour <= 17; $hour++){
-												$minutes =15;
+												$minutes =0;
+												$minutes = str_pad($minutes, 2, '0', STR_PAD_LEFT);
 												if($hour == 17){
 														$minutes =0;
 														$minutes = str_pad($minutes, 2, '0', STR_PAD_LEFT);
-													 echo'<option value=" ='. $hour . ' ":" '.$minutes.'">' . $hour .':' .$minutes . '</option>';
+													 echo'<option value="'. $hour . '.'.$minutes.'">' . $hour .'.' .$minutes . '</option>';
 												}else {
 													while( $minutes < 60 ){
-												 	if($hour <=10){
-												 		$hour = str_pad($hour, 2, '0', STR_PAD_LEFT);
-												 	}
-												 	 echo'<option value=" ='. $hour . ' ":" '.$minutes.'">' . $hour .':' .$minutes . '</option>';
-												 	 $minutes+=15;
+															if($hour <=10){
+																$hour = str_pad($hour, 2, '0', STR_PAD_LEFT);
+															}
+														 echo'<option value="'. $hour . '.'.$minutes.'">' . $hour .'.' .$minutes . '</option>';
+														 $minutes+=15;
 												 	}
 												}
 											}
