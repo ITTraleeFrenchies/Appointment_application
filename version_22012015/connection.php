4efecc78c1b717fd7a9ac1a06b9d530cdd844e77
kyalha <angel079@hotmail.fr>
2015-02-06 09:58:49 +0000

    Reset

--- version_22012015/connection.php
+++
@@ -1,9 +0,0 @@
-<?php  
-	//================= identifications for database =================
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "db";
-	//================= Create connection =================
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-?>	
